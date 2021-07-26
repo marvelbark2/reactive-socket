44e1bc697070bbcf674eba925b8a812e036db6e1
Youness Masaoudi <marvelbark2@gmail.com>
2021-07-26 17:40:26 +0200

    Commit other Version

--- src/main/java/ws/prospeak/project/socket/reactivex/server/observer/Test.java
+++ src/main/java/ws/prospeak/project/socket/reactivex/server/observer/Test.java
@@ -1,11 +1,20 @@
 package ws.prospeak.project.socket.reactivex.server.observer;
 
+import io.reactivex.Observable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.concurrent.Flow;
 
 public class Test {
+    final static Logger logger = LoggerFactory.getLogger(Test.class);
     public static void main(String[] args) {
        Server server = new Server(61022);
        server.subscribe(Test::log);
@@ -16,6 +25,27 @@ public class Test {
         try {
           PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
           writer.println("Hello there");
+          InputStream reader = socket.getInputStream();
+          var t = Observable.defer(() -> {
+              byte[] transBuffer = new byte[1028];
+              Runnable read = () -> {
+                  try {
+                      var d = reader.read(transBuffer);
+                  } catch (IOException ioException) {
+                      logger.error(ioException.getMessage(), ioException);
+                  }
+              };
+              read.run();
+              Byte[] buffer = new Byte[1028];
+              int i=0;
+              for (byte buf: transBuffer) {
+                  buffer[i++] = buf;
+              }
+              return Observable.fromArray(buffer);
+          }).repeat().takeWhile(x -> x > 0).subscribe(x -> {
+              logger.info("Byte: {}", x);
+              writer.println("Byte is " + x);
+          });
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
